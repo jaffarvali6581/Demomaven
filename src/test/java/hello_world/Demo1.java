63f5481a6cabc0489216cf59d33b1278471920f6
jaffar <jaffarkattupodi3533@gmail.com>
2021-11-12 22:59:49 +0530

    adding window size

--- src/test/java/hello_world/Demo1.java
+++ src/test/java/hello_world/Demo1.java
@@ -1,5 +1,7 @@
 package hello_world;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -11,6 +13,8 @@ public class Demo1 {
 	public void test1() {
 		driver=new ChromeDriver();
 		driver.get("https:/www.Google.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 }
